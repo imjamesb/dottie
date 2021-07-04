1b55b02f67389ea97372175d64bd8de1fd004896
James Bradlee <james@ihacks.dev>
2021-07-04 08:53:13 +0200

    checkpoint

--- cli/commands/upgrade.ts
+++ cli/commands/upgrade.ts
@@ -1,6 +1,10 @@
 // Imports
 import type { DotOpts } from "./mod.ts";
+import { valid } from "https://deno.land/x/semver@v1.4.0/mod.ts";
+import { Cash } from "https://deno.land/x/cash@0.1.0-alpha.13/mod.ts";
 import { Command } from "../deps.ts";
+import cversion from "../../version.ts";
+import DotError from "../../lib/DotError.ts";
 
 export type UpgradeOpts = {
   canary?: true;
@@ -8,7 +12,22 @@ export type UpgradeOpts = {
 
 export type UpgradeArgs = [version?: string];
 
-function _target(version: string) {
+export function tagToSemver(tags: string): string | null {
+  const parts: string[] = tags.split("/");
+  if (parts.length == 3) {
+    if (!(parts[1] = valid(parts[1])!)) {
+      throw new Error("Invalid semver version!");
+    }
+    if (parts[0] !== "canary") throw new Error("Invalid semver extension!");
+    if (!/^\d+$/g.test(parts[2])) {
+      throw new Error("Invalid semver extension version!");
+    }
+    return parts[1] + "-" + parts[0] + "." + parts[2];
+  } else if (parts.length === 1) return parts[0];
+  else throw new Error("Invalid tag!");
+}
+
+function target(version: string) {
   return `https://github.com/ihack2712/dottie/releases/download/${version}/dot-${Deno.build.target}.zip`;
 }
 
@@ -18,11 +37,43 @@ function versionList(canary: boolean) {
   }.txt`;
 }
 
+function versionLatest(canary: boolean) {
+  return `https://raw.githubusercontent.com/ihack2712/dottie/install-map/latest${
+    canary ? "-canary" : ""
+  }.txt`;
+}
+
 async function getVersions(canary = false): Promise<string[]> {
   return (await (await fetch(versionList(canary))).text()).trim().split(/\s+/);
 }
 
+async function getLatestVersion(canary = false): Promise<string> {
+  return (await (await fetch(versionLatest(canary))).text()).trim();
+}
+
 const _mode = 33279;
+
+function download(
+  from: string,
+  to: string,
+  showProgress = true,
+): Promise<void> {
+  return new Promise<void>((resolve, reject) =>
+    fetch(from).then((response) => {
+      if (!response.ok) {
+        return reject(new Error("Request did not return an ok response code."));
+      }
+      if (!response.body) {
+        return reject(new Error("Request did not return a body!"));
+      }
+      const reader = response.body.getReader();
+      const bytes = Number(response.headers.get("content-type"));
+      if (bytes < 1) {
+        return Deno.writeTextFile(to, "").then(resolve).catch(reject);
+      }
+    }).catch(reject)
+  );
+}
 
 export default new Command<UpgradeOpts, UpgradeArgs, DotOpts>()
   .name("upgrade")
@@ -37,12 +88,31 @@ export default new Command<UpgradeOpts, UpgradeArgs, DotOpts>()
       try {
         console.log(" - %s", (await getVersions(!!opts.canary)).join("\n - "));
       } catch {
-        console.error("error: Could not fetch version list!");
+        throw new DotError("Could not fetch version list!");
       }
     },
   })
-  .action(async (_opts, _version) => {
-    console.error("error: Not implemented!");
-    await Promise.resolve();
-    Deno.exit(1);
+  .action(async (opts, _version) => {
+    let version!: string;
+    let tag!: string;
+    try {
+      tag = await getLatestVersion(!!opts.canary);
+      version = tagToSemver(tag)!;
+      if (!tag) throw 1;
+    } catch {
+      throw new DotError("Could not fetch version list!");
+    }
+    const $ = new Cash();
+    $.verbose = opts.verbose ?? $.verbose;
+    if (opts.verbose) console.log("Checking for unzip!");
+    try {
+      await $`command -v unzip`;
+    } catch (err) {
+      console.log(err);
+      throw new Error("unzip is required to upgrade!");
+    }
+    if (opts.verbose) {
+      console.log("Upgrading from '%s' to '%s'", cversion, version);
+    }
+    await download(target(version), Deno.env.get("HOME") + "/.dotbin/dot.zip");
   });
